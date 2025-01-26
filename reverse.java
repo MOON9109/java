195e169c8aa1d4524e3f75c3d997a0a92690f12f
moon91 <hanhannetnet@naver.com>
2025-01-26 21:43:13 +0900

    Add StringBuilder code

---
+++ reverse.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Main{
+
+   public ArrayList<String>  Soution (int n, String[] str){
+       ArrayList<String> answer= new ArrayList<>();
+       for (String x :str){
+
+           //StringBuilder를 만들면 새로운 객체를 만들지 않고서 처리 가능
+           String temp =new StringBuilder(x).reverse().toString();
+           answer.add(temp);
+
+       }
+
+       return answer;
+   }
+
+
+
+
+public static void main(String[]args) {
+
+    Main T = new Main();
+    Scanner kb = new Scanner(System.in);
+    int n = kb.nextInt();
+    String[] str = new String[n];
+
+    //배열에 하나씩 넣음
+    for (int i = 0; i < n; i++) {
+
+        str[i] = kb.next();
+    }
+
+    for (String x: T.Soution(n,str)){
+        System.out.println(x);
+
+    }
+
+
+   }
+}
