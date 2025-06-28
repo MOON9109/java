c430bf84c1764feeb2ed00a6750e51a398a2a8bb
moon91 <hanhannetnet@naver.com>
2025-06-29 00:39:45 +0900

    Add charArray code

---
+++ Sum(CharArray).java
@@ -0,0 +1,23 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+        int N= sc.nextInt();
+        String sNum=sc.next();
+        char[] cNum = sNum.toCharArray();
+        int sum=0;
+
+        for (int i=0; i<cNum.length; i++){
+            sum+= cNum[i] -'0';
+
+        }
+
+        System.out.println(sum);
+
+    }
+}
